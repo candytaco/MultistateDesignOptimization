2a73880cf46a73c319f3906bb5190e2226a68c40
Tianjiao Zhang <candytaco@outlook.com>
2016-04-15 14:46:08 -0700

    updated skeleton

--- MultistateDesignOptimization/MultistateDesignOptimization/optimizerC/Optimizer.cpp
+++ MultistateDesignOptimization/MultistateDesignOptimization/optimizerC/Optimizer.cpp
@@ -26,3 +26,32 @@ Optimizer::Optimizer(int nMacrostates, string *macrostates, bool continuousBoltz
 {
 	// TODO: implement this
 }
+
+void Optimizer::readData(string *inFile)
+{
+
+}
+
+void Optimizer::readMicrostateData(string *inFile)
+{
+
+}
+
+void Optimizer::writeFrequenciesToFASTA(string *outName, int precision, float **frequencies)
+{
+
+}
+
+void Optimizer::writeBestParamsToText(string *outName)
+{
+
+}
+
+int Optimizer::calcParamsID(double param1, double param2, double param3)
+{
+	return NULL;
+}
+Model *Optimizer::getModelByParams(double, double, double)
+{
+	return NULL;
+}
